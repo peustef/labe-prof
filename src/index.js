3f083fc405f2d3782bb813c6ceef35a66bad16a5
Renato Marinho <renatomarinho@pop-os.localdomain>
2021-05-26 08:32:58 -0300

    adicionado tailwindcss no projeto

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Cadastro from './pages/Cadastro';
-import Carrinho from './pages/Carrinho';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./pages/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+import Carrinho from "./pages/Carrinho";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact={true} component={Home} />
-      <Route path="/Cadastro"  component={Cadastro} />
-      <Route path="/Carrinho"  component={Carrinho} />      
+      <Route path="/cadastro" component={Cadastro} />
+      <Route path="/carrinho/:produtosCarrinho" component={Carrinho} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
