15babbc37f5eb6ccce5222fe6de5e71af35592bc
peustef <peustefanis@gmail.com>
2021-05-26 12:53:17 -0300

    Logo centralizado

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,32 +8,29 @@ import logo from "../../img/logo.png"
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className="w-full grid grid-cols-3 bg-white shadow-md items-center justify-center md:flex-row">
 
-        <div className="w-full">
-          <nav className="bg-white shadow-md">
-            <div className="md:flex items-center justify-between py-2 px-8 ">
-              <div className="flex justify-between items-center">
-                <div className=" w-64 font-bold text-gray-800 ">
-                  <Link to="/">
-                    <button><img src={logo} /></button>
-                  </Link>
-                </div>
-              </div>
-
-              <div class="flex flex-col md:flex-row  ">
-                <Link to="/cadastro" className="text-gray-800 rounded hover:bg-blue-800 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
-                  <button>Dar aulas</button>
-                </Link>
-                <Link to="/carrinho/:produtosCarrinho" className="text-gray-800 rounded hover:bg-blue-800 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
-                  <button>
-                    <HiShoppingCart />
-                  </button>
+        <div className="col-start-2">
+          <div className=" md:flex items-center py-2 px-8 ">
+            <div className=" flex items-center">
+              <div className=" w-64 font-bold text-gray-800 ">
+                <Link to="/">
+                  <a><img src={logo} /></a>
                 </Link>
               </div>
-
             </div>
-          </nav>
+          </div>
+        </div>
+
+        <div className="col-start-3 flex justify-center items-center md:flex-row">
+          <Link to="/cadastro" className="text-gray-800 rounded hover:bg-blue-800 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
+            <button>Dar aulas</button>
+          </Link>
+          <Link to="/carrinho/:produtosCarrinho" className="text-gray-800 rounded hover:bg-blue-800 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">
+            <button>
+              <HiShoppingCart />
+            </button>
+          </Link>
         </div>
 
 
