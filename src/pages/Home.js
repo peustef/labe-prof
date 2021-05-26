ad0b472052256f3d4e3cbd2f811d4263b039259b
Renato Marinho <renatomarinho@pop-os.localdomain>
2021-05-26 12:14:03 -0300

    adicionado layout do filtro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Filtro from '../components/Filtro/Filtro'
 
 export default class Home extends React.Component {
 	render() {
 		return (
+			<div>
 				<Header />
-
+				<Filtro />
+			</div>
 				
 			)
 	}
