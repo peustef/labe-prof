cc1f9f6967517681495eaae3b7b64800b2801ff6
Renato Marinho <renatomarinho@pop-os.localdomain>
2021-05-27 08:01:34 -0300

    Cards estão puxando os dados da API

--- src/components/CardPost/CardPost.js
+++ src/components/CardPost/CardPost.js
@@ -1,39 +1,92 @@
 import React from "react";
 import { HiShoppingCart } from "react-icons/hi";
-
+import axios from "axios";
 
 export default class CardPost extends React.Component {
-    state = {
-        title: "Cortar a grama",
-        description: "Manutenção em áreas verdes de até 1000 metros quadrados.",
-        price: 40,
-        paymentMethods: ["PayPal", "boleto"],
-        dueDate: "2021-12-30"
+  state = {
+    servicos: []
+  };
+
+  componentDidMount() {
+    this.getAllJobs();
+  }
+
+  getAllJobs = async () => {
+    const URL = "https://labeninjas.herokuapp.com/jobs";
+    const headers = {
+      headers: { Authorization: "6b5d3ade-aeb6-4364-91fa-b9a319e476c5" }
+    };
+    try {
+      const res = await axios.get(`${URL}`, headers);
+      this.setState({ servicos: res.data.jobs });
+      console.log(this.state.servicos);
+    } catch (error) {
+      alert(error);
     }
-    render() {
-        return (
+  };
 
-            <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden my-10 ml-8 bg-gray-200 ">
-                <div className="px-4 py-2">
-                    <h1 className="text-blue-700 font-bold text-2xl uppercase  ">{this.state.title}</h1>
-                    <p className="text-blue-700 text-sm mt-1">{this.state.description}</p>
-                </div>
-                <img className="h-56 w-full object-cover mt-2" src="https://blog.academia.com.br/wp-content/uploads/2019/02/273098-entenda-qual-e-o-papel-do-professor-na-educacao-dos-alunos.jpg" alt="PROFESSORA" />
-                <div className="flex items-center justify-between px-4 py-2 bg-gray-200">
-                    <p className="text-blue-700 text-xl">{this.state.dueDate}</p>
-                    <p className="text-blue-700 text-xl">{this.state.paymentMethods}</p>
-                </div>
-                <div className="flex items-center justify-between px-4 py-2 bg-gray-200">
-                    <h1 className="text-blue-700 font-bold text-xl">R${this.state.price},00</h1>
+  updateJob = (jobId) => {
+    const URL = "https://labeninjas.herokuapp.com/jobs";
+    const body = {
+      taken: true
+    };
+    const headers = {
+      headers: { Authorization: "6b5d3ade-aeb6-4364-91fa-b9a319e476c5" }
+    };
+    axios
+      .post(`${URL}/${jobId}`, body, headers)
+      .then((res) => {
+        alert("Aula adicionada ao carrinho");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
-                    <button class="px-6 py-2 bg-blue-600 text-white text-lg  font-semibold rounded-full hover:bg-blue-800 inline-flex items-center ">                        
-                    <HiShoppingCart />Adicionar
+  render() {
+    const filterJobs = this.state.servicos.filter((servico) => {
+      if (servico.taken === false) {
+        return true;
+      }
+    });
 
-                    </button>
-                </div>
-            </div >
+    const jobs = filterJobs.map((job) => {
+      return (
+        <div
+          key={job.id}
+          className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden my-10 ml-8 bg-gray-200 "
+        >
+          <div className="px-4 py-2">
+            <h1 className="text-blue-700 font-bold text-2xl uppercase  ">
+              {job.title}
+            </h1>
+            <p className="text-blue-700 text-sm mt-1">{job.description}</p>
+          </div>
+          <img
+            className="h-56 w-full object-cover mt-2"
+            src="https://blog.academia.com.br/wp-content/uploads/2019/02/273098-entenda-qual-e-o-papel-do-professor-na-educacao-dos-alunos.jpg"
+            alt="PROFESSORA"
+          />
+          <div className="flex items-center justify-between px-4 py-2 bg-gray-200">
+            <h1 className="text-blue-700 font-bold text-xl">
+              R${job.price},00
+            </h1>
 
-
-        )
-    }
+            <button
+              class="px-6 py-2 bg-blue-600 text-white text-lg  font-semibold rounded-full hover:bg-blue-800 inline-flex items-center "
+              onClick={() => this.updateJob(job.id)}
+            >
+              <HiShoppingCart />
+              Adicionar
+            </button>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 justify-center items-center">
+        {jobs}
+      </div>
+    );
+  }
 }
