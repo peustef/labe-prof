3f80b27842d365077bc333c0a36ff05059dee2fb
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-05-28 11:06:39 -0300

    tela cadastro com responsividade

--- src/components/Form-cadastro/slyted.js
+++ src/components/Form-cadastro/slyted.js
@@ -7,11 +7,30 @@ export const ContainerForm = styled.div`
   gap: 2em;
   width: 60%;
   height: 100%;
+
+  h2 {
+    font-size: 24px;
+    font-weight: 600;
+  }
+
+  input {
+    padding: 6px;
+    border-radius: 5px;
+  }
+
+  @media(max-width: 700px){
+    width: 100%;
+  } 
 `
 
 export const FormLine = styled.div`
   display: flex; 
-  justify-content: space-between ;        
+  justify-content: space-between ;   
+
+  @media(max-width: 700px){
+    display: flex;
+    flex-direction: column;
+  }     
 `
 export const FormInfo = styled.div`
   display: flex; 
--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -22,6 +22,10 @@ const ContainerImg = styled.div`
 const ImgLateral = styled.img`
   max-height: 100%;
   max-width: 100%;
+
+  @media(max-width: 700px){
+    display: none;
+  }
 `
 
 export default class Cadastro extends React.Component {
