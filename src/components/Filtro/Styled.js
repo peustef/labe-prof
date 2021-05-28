6887a3e88caedd4df01facde3c5626bd19287070
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-05-28 11:33:28 -0300

    ajuste de input do filtro

--- src/components/Filtro/Styled.js
+++ src/components/Filtro/Styled.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 export const ContainerFiltro = styled.div`
+    display: flex;
+    flex-direction: row;
+
     select {
-        padding: 7px;
+        padding: 5px;
         border-radius: 5px;
         outline: none;
     }
+
+    
 
     @media(max-width: 700px){
         display: flex;
@@ -21,12 +26,28 @@ export const ContainerFiltro = styled.div`
 `
 
 export const Pesquisa = styled.div`
+    margin-right: 10px;
+    
+    input{
+        padding: 6px 40px;
+        border-radius: 5px;
+    }
+
     @media(max-width: 700px){
         width: 95vw;
         margin-bottom: 3px;
+        margin-right: 0;
 
         input {
             width: 100%;
         }
+    }
+`
+
+export const ContainerOrdenacao = styled.div`
+    input{
+        margin-left: 2px;
+        padding: 6px;
+        border-radius: 5px;
     }
 `
