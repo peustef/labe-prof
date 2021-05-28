252f8dc9fc935a4bffb0595d15a3d9be9d82b79d
Renato Marinho <renatomarinho@pop-os.localdomain>
2021-05-28 14:55:42 -0300

    adicionado animacoes de entrada dos cards

--- src/components/CardPost/Styled.js
+++ src/components/CardPost/Styled.js
@@ -12,10 +12,22 @@ export const Card = styled.div`
     height: 440px;
     justify-content: space-evenly;
     align-content: space-between;
-
     /* faz quebrar a linha com ... */
     flex-wrap: wrap;
     min-width: 0;
+    animation: fade-in-right 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+    @keyframes fade-in-right {
+  0% {
+    transform: translateX(50px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+
 
     p{
         /* para não ter barra de rolagem */
